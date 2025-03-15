e074a9338569e163c8cab7658ca437cc762c7749
julzedz <edoziechukwuma@gmail.com>
2025-03-15 22:49:05 +0100

    Add product view and route with authentication middleware

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,10 @@ Route::view('dashboard', 'dashboard')
     ->middleware(['auth', 'verified'])
     ->name('dashboard');
 
+Route::view('product', 'product')
+    ->middleware(['auth', 'verified'])
+    ->name('product');
+
 Route::middleware(['auth'])->group(function () {
     Route::redirect('settings', 'settings/profile');
 

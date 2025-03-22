3f9c618ba18fd2ef69206976d52b8c7db23a7f92
julzedz <edoziechukwuma@gmail.com>
2025-03-22 10:47:53 +0100

    Add Prettier for formatting php files

--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -1,21 +1,20 @@
 @extends('components.layouts.main')
 
-    @section('header')
+@section('header')
     <h2>Product11</h2>
 @endsection
 
 @section('content')
-    <form action={{ route("formsubmitted") }} method="post">
-    @csrf
-    <label for="fullname">Full Name:</label>
-    <input class="" type="text" id="fullname" name="fullname" placeholder="Name" required>
-    <br>
-    <label for="email">E-mail:</label>
-    <input type="text" id="email" name="email" placeholder="Email" required>
-    <br>
-    <button type="submit">Submit</button>
-  </form>
-  @endsection
-
-</body>
-</html>
+    <form action="{{ route('formsubmitted') }}" method="post">
+        @csrf
+        <label for="fullname">Full Name:</label>
+        <input class="" type="text" id="fullname" name="fullname" placeholder="Name" required>
+        <br>
+        <br>
+        <label for="email">E-mail:</label>
+        <input type="text" id="email" name="email" placeholder="Email" required>
+        <br>
+        <br>
+        <button type="submit">Submit</button>
+    </form>
+@endsection

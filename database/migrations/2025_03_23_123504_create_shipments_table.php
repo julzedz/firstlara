4b875946ab3182c400a8571c5118565c5189bcdd
julzedz <edoziechukwuma@gmail.com>
2025-03-23 14:03:48 +0100

    Add Shipment model and migration

---
+++ database/migrations/2025_03_23_123504_create_shipments_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('shipments', function (Blueprint $table) {
+            $table->id();
+            $table->dateTime('shipment_date');
+            $table->string('address', 100);
+            $table->string('city', 50);
+            $table->string('state', 50);
+            $table->string('country', 50);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('shipments');
+    }
+};

3397984ff2f80319c49dcf0131fbf685f8cddaae
julzedz <edoziechukwuma@gmail.com>
2025-03-25 16:27:16 +0100

    Add fillable attributes and relationships to Payment model

--- app/Models/Payment.php
+++ app/Models/Payment.php
@@ -2,9 +2,24 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Payment extends Model
 {
     //
+
+    use HasFactory;
+
+    protected $fillable = [ 'payment_date', 'payment_method', 'amount', 'user_id' ];
+
+    public function user()
+    {
+      return $this->belongsTo(User::class);
+    }
+
+    public function orders()
+    {
+      return $this->hasMany(Order::class);
+    }
 }

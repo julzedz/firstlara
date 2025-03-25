6a1cbf88d668351666ffeabf5773d0f6a262f700
julzedz <edoziechukwuma@gmail.com>
2025-03-25 16:11:13 +0100

    Add relationships and fillable attributes to OrderItem model

--- app/Models/OrderItem.php
+++ app/Models/OrderItem.php
@@ -2,9 +2,26 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class OrderItem extends Model
 {
     //
+
+    use HasFactory;
+
+    protected $fillable = [
+      'quantity', 'price', 'product_id', 'order_id'
+    ];
+
+    public function order()
+    {
+      return $this->belongsTo(Order::class);
+    }
+
+    public function product()
+    {
+      return $this->belongsTo(Product::class);
+    }
 }

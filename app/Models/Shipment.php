ab8fcaae3f259e3429c81cad7504062595c2cfd5
julzedz <edoziechukwuma@gmail.com>
2025-03-25 16:37:07 +0100

    Add fillable attributes and relationships to Shipment model

--- app/Models/Shipment.php
+++ app/Models/Shipment.php
@@ -2,9 +2,24 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Shipment extends Model
 {
     //
+
+    use HasFactory;
+
+    protected $fillable = [ 'shipment_date', 'address', 'city', 'state', 'country', 'user_id' ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function orders()
+    {
+        return $this->hasMany(Order::class);
+    }
 }

838b3a647c19d9e179085cbc1b8e7bb34770ab44
julzedz <edoziechukwuma@gmail.com>
2025-03-26 08:42:49 +0100

    Update test data to include address field.

--- tests/Feature/Auth/AuthenticationTest.php
+++ tests/Feature/Auth/AuthenticationTest.php
@@ -13,7 +13,9 @@ test('login screen can be rendered', function () {
 });
 
 test('users can authenticate using the login screen', function () {
-    $user = User::factory()->create();
+    $user = User::factory()->create([
+      'address' => '123 Main St',
+    ]);
 
     $response = Livewire::test(Login::class)
         ->set('email', $user->email)
@@ -28,7 +30,9 @@ test('users can authenticate using the login screen', function () {
 });
 
 test('users can not authenticate with invalid password', function () {
-    $user = User::factory()->create();
+    $user = User::factory()->create([
+      'address' => '123 Main St',
+    ]);
 
     $this->post('/login', [
         'email' => $user->email,
@@ -39,7 +43,9 @@ test('users can not authenticate with invalid password', function () {
 });
 
 test('users can logout', function () {
-    $user = User::factory()->create();
+    $user = User::factory()->create([
+      'address' => '123 Main St',
+    ]);
 
     $response = $this->actingAs($user)->post('/logout');
 

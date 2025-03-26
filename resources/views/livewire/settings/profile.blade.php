21bc61e528fd9336ff3a757a36dfa66f1ed523c4
julzedz <edoziechukwuma@gmail.com>
2025-03-26 16:27:36 +0100

    Update profile form to include address and phone number fields

--- resources/views/livewire/settings/profile.blade.php
+++ resources/views/livewire/settings/profile.blade.php
@@ -1,7 +1,7 @@
 <section class="w-full">
     @include('partials.settings-heading')
 
-    <x-settings.layout :heading="__('Profile')" :subheading="__('Update your name and email address')">
+    <x-settings.layout :heading="__('Profile')" :subheading="__('Update your personal information')">
         <form wire:submit="updateProfileInformation" class="my-6 w-full space-y-6">
             <flux:input wire:model="name" :label="__('Name')" type="text" required autofocus autocomplete="name" />
 
@@ -26,6 +26,22 @@
                     </div>
                 @endif
             </div>
+
+            <flux:input 
+                wire:model="address" 
+                :label="__('Address')" 
+                type="text" 
+                required 
+                autocomplete="street-address" 
+            />
+
+            <flux:input 
+                wire:model="phone_number" 
+                :label="__('Phone Number')" 
+                type="tel" 
+                required 
+                autocomplete="tel" 
+            />
 
             <div class="flex items-center gap-4">
                 <div class="flex items-center justify-end">

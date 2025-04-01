7bea477a2285377abd8a9e0ea3d617c5097747d2
julzedz <edoziechukwuma@gmail.com>
2025-04-01 14:41:28 +0100

    Add store method to OrderController.

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -3,8 +3,20 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Order;
 
 class OrderController extends Controller
 {
     //
+    public function store(Request $request)
+    {
+      $order = Order::create([
+        'user_id' => auth()->id(),
+        'order_date' => now(),
+        'total_price' => $request->total_price,
+        'payment_id' => $request->payment_id,
+        'shipment_id' => $request->shipment_id
+      ]);
+      return response()->json(['message' => 'Order created!', 'order' => $order]);
+    }
 }

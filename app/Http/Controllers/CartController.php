018c367d6168c3ff49652f7c961474ed53f9ab12
julzedz <edoziechukwuma@gmail.com>
2025-04-01 14:57:57 +0100

    Add store method to CartController for updating cart items

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -2,9 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Cart;
 use Illuminate\Http\Request;
 
 class CartController extends Controller
 {
     //
+    public function store(Request $request)
+    {
+      $cart = Cart::updateOrCreate(
+        ['user_id' => auth()->id(), 'product_id' => $request->product_id],
+        ['quantity' => $request->quantity]
+      );
+      return response()->json(['message' => 'Cart updated!', 'cart' => $cart]);
+    }
 }

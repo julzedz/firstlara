4d624203355aa4585d08453955fc33c81b7da5b1
julzedz <edoziechukwuma@gmail.com>
2025-04-01 15:54:11 +0100

    Add processPayment method to PaymentController.

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -2,9 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Payment;
 use Illuminate\Http\Request;
 
 class PaymentController extends Controller
 {
     //
+    public function processPayment(Request $request)
+    {
+      $payment = Payment::create([
+        'user_id' => auth()->id(),
+        'payment_date' => now(),
+        'amount' => $request->amount,
+        'payment_method' => $request->payment_method
+      ]);
+      return response()->json(['message' => 'Payment successful!', 'payment' => $payment]);
+    }
 }

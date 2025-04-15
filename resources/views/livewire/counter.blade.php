e73b851f73cce15a319c1c4cc811b364ff01238d
julzedz <edoziechukwuma@gmail.com>
2025-04-15 17:16:23 +0100

    Create counter livewire component

---
+++ resources/views/livewire/counter.blade.php
@@ -0,0 +1,9 @@
+<div>
+    {{-- Be like water. --}}
+    <h1>
+      {{ $count }}
+
+      <button wire:click="increment">+</button>
+      <button wire:click="decrement">+</button>
+    </h1>
+</div>

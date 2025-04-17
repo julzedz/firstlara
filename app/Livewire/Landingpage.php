1c3df6ac90fce21a09c943e28e27d23c3c263f1e
julzedz <edoziechukwuma@gmail.com>
2025-04-17 16:27:00 +0100

    Fetch category and product in landingpage livewire component

--- app/Livewire/Landingpage.php
+++ app/Livewire/Landingpage.php
@@ -3,9 +3,28 @@
 namespace App\Livewire;
 
 use Livewire\Component;
+use App\Models\Category;
+use App\Models\Product;
 
 class Landingpage extends Component
 {
+  public $categories;
+  public $products;
+
+
+
+  public function mount()
+  {
+    try {
+        $this->categories = Category::get();
+        $this->products = Product::get();
+    } catch (\Exception $e) {
+        $this->categories = collect();
+        $this->products = collect();
+        // Optionally log the error or handle it as needed
+        \Log::error('Error fetching categories or products: ' . $e->getMessage());
+    }
+  }
     public function render()
     {
         return view('livewire.landingpage');
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Livewire\Landingpage;
 use App\Livewire\Settings\Appearance;
 use App\Livewire\Settings\Password;
 use App\Livewire\Settings\Profile;
@@ -8,6 +9,8 @@ use Illuminate\Http\Request;
 use App\Livewire\Counter;
 
 Route::get('/counter', Counter::class);
+
+Route::get('/landingpage', Landingpage::class)->name('landingpage');
 
 Route::get('/', function () {
     return view('welcome');
